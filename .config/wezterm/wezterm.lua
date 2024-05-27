35c9236853c0c349a2b71acb76f954fa85328957
René van Balen <rene.van.balen@ictivity.nl>
2024-05-28 00:18:49 +0200

    Add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = true,
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 14.0,
+
+	window_background_opacity = 0.92,
+	window_decorations = "RESIZE",
+}
